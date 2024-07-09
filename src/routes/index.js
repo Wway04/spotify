a4a10d98df454c2b690558ed3e0a76ade1889859
quockhanh04 <22520637@gm.uit.edu.vn>
2024-07-09 16:25:09 +0700

    feat: Add public and private routes
    This commit adds public and private routes to the application. The public routes include the home, search, audio, login, and register pages. The private routes are currently empty. This change allows for proper navigation and access control within the application.

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+import PAGES from "../pages";
+
+const publicRoutes = [
+  { path: process.env.REACT_APP_HOME, component: PAGES.home, mainLayout: true },
+  { path: process.env.REACT_APP_SEARCH, component: PAGES.search, mainLayout: true },
+  { path: process.env.REACT_APP_AUDIO, component: PAGES.audio, mainLayout: true },
+  { path: process.env.REACT_APP_LOGIN, component: PAGES.login, mainLayout: false },
+  { path: process.env.REACT_APP_REGISTER, component: PAGES.register, mainLayout: false },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
